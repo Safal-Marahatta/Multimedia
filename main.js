60752029a570b0d353f8b41dbba91c11d1484b06
Safal-Marahatta <safalmarahatta12456@gmail.com>
2024-02-27 21:45:38 +0545

    Initial commit

---
+++ main.js
@@ -0,0 +1,64 @@
+const navMenu = document.getElementById("nav-menu"),//tyo thulo view huda ko navbar ko id ho
+  navToggle = document.getElementById("nav-toggle"),
+  navItem = document.querySelectorAll(".nav__item"),
+  header = document.getElementById("header");
+
+// open and close menu
+navToggle.addEventListener("click", () => {//yo tyo mobile view ko lagi ho.tyo icon matra aauxa ani teslai click garesi list banna paryo ni teako lagi tyo element ko class change gardiya
+  navMenu.classList.toggle("nav__menu--open");
+  changeIcon();
+});
+
+// close the menu when the user clicks the nav links
+navItem.forEach((item) => {//gastai mobile view ma list display vaxa different section ko teslai click garesi tyo section ma gana paryo
+  item.addEventListener("click", () => {
+    if (navMenu.classList.contains("nav__menu--open")) {
+      navMenu.classList.remove("nav__menu--open");
+    }
+    changeIcon();
+  });
+});
+
+// Change nav toggle icon
+function changeIcon() {
+  if (navMenu.classList.contains("nav__menu--open")) {
+    navToggle.classList.replace("ri-menu-3-line", "ri-close-line");
+  } else {
+    navToggle.classList.replace("ri-close-line", "ri-menu-3-line");
+  }
+}
+
+
+// header scroll animation
+window.addEventListener("scroll", () => {
+  if (window.scrollY > 40) {
+    header.classList.add("header--scroll");
+  } else {
+    header.classList.remove("header--scroll");
+  }
+});
+
+// ScrollReveal animations
+const sr = ScrollReveal({//. ScrollReveal is a JavaScript library that allows you to animate elements as they become visible during scrolling. 
+  duration: 2000,
+  distance: "100px",
+  delay: 400,
+  reset: false,
+});
+
+sr.reveal(".hero__content, .about__content");
+sr.reveal(".hero__img", { origin: "top" });
+
+sr.reveal(
+  ".hero__info-wrapper, .skills__title, .skills__content, .qualification__name, .qualification__item, .service__card, .project__content, .testimonial__wrapper, .footer__content",
+  {
+    delay: 500,
+    interval: 100,
+  }
+);
+
+sr.reveal(".qualification__footer-text, .contact__content", {
+  origin: "left",
+});
+
+sr.reveal(".qualification__footer .btn, .contact__btn", { origin: "right" });
